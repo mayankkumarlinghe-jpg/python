01d1743e0da1da7ee21bdc456d051ee8fc99a0c8
mayankkumarlinghe-jpg <mayankkumarlinghe@gmail.com>
2026-02-28 21:51:52 +0530

    Add input handling and conditional statements examples

--- basics of python.py
+++ basics of python.py
@@ -17,3 +17,49 @@ print(type(3!=2))
 print(type(3>=2))
 print(type(3>2.0))
 # taking input in python
+x=input("Enter a string input")
+y=int(input("Enter a integer input"))
+z=float(input("Enter a float input"))
+q=int(input("Enter a integer input"))
+print(x)
+print(y)
+print(z)
+print(q)
+#this is a single line comment in python
+""" this is a multiline
+comment in python"""
+''' another way for multiline 
+comments in python'''
+
+#conditional statements in python:
+# if statement
+if condition:#if condition is true statement executes the if block else the most next statement after the if block
+  print(x)
+#if-else
+if condition:
+  statement
+else: 
+  statement 
+  # if block executes if the if condition is true else else statement if be executed bothcan't at same time and else can't be used without else
+#if-elif
+if condition:
+  statement
+elif condition:
+  statement
+elif condition:
+  statement
+else:
+  statement 
+ # when we want to check diffrerent condition parallely independently checking multiple things then we use if-elif-else statements 
+
+#nested if else:
+ if condition:
+   if condition:
+     statement
+ else:
+   if condition:
+     statement
+    #using inner and outer if else and making it nested is using the indentation in python when we want multiple conditions to be checked dependently on the outer conditions 
+
+
+  

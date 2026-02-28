c2aa83a895a923fd17781252cbe27b9bb1657255
mayankkumarlinghe-jpg <mayankkumarlinghe@gmail.com>
2026-02-28 21:40:56 +0530

    Add basic Python data type examples

---
+++ basics of python.py
@@ -0,0 +1,19 @@
+a=10
+b=20
+print(a+b)
+#usage of different types of data type
+print(type(3>2))
+print(type(3))
+print(type(3.00))
+print(type(True))
+print(type("String"))
+print(type(list[]))
+dict={}
+print(type(dict))
+print(type(33/3))
+print(type(3.0/2))
+
+print(type(3!=2))
+print(type(3>=2))
+print(type(3>2.0))
+# taking input in python

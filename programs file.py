edd6419bd02b46fb825a9d2295d36fbd4d5a4649
mayankkumarlinghe-jpg <mayankkumarlinghe@gmail.com>
2026-02-28 22:04:10 +0530

    Implement number positivity check and grading algorithm
    Added a program to check if a number is positive and designed an algorithm to determine student grades based on their score using nested if-else statements.

---
+++ programs file.py
@@ -0,0 +1,26 @@
+#Write a program to check whether a number is positive.
+num = int(input("Enter Number"))
+if num>0:
+    print("Number is positive")
+elif num<0:
+    print("Number is negative")
+else:
+    print("Number is 0")
+"""Design an algorithm using nested if-else and elif statements to determine the grade of
+a student based on their score. The grading scale is: A for 90-100, B for 80-89, C for 70-79, D for 60-69,
+and F for below 60.
+"""
+marks = int(input("Enter the marks of student")
+if marks > 0:
+            if marks => 90 and marks <=100:
+                print("Grade A")
+            elif marks = >80 and marks <= 89:
+                print("Grade B")
+            elif marks => 70 and marks <= 79:
+                print("Grade C")
+            elif marks => 60 and <= 69:
+                print("Grade D")
+            elif marks < 60:
+                print("Grade F")
+else:
+    print("Enter valid amrks")

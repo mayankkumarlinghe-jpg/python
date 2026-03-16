61d4cd2b3fcb45a2908c98503400f85035ed34e2
mayankkumarlinghe-jpg <mayankkumarlinghe@gmail.com>
2026-03-16 12:00:00 +0530

    Fix IndentationError and NameError in basics of python

--- basics of python.py
+++ basics of python.py
@@ -33,32 +33,32 @@ comments in python'''
 
 #conditional statements in python:
 # if statement
-if condition:#if condition is true statement executes the if block else the most next statement after the if block
-  print(x)
+# if condition:#if condition is true statement executes the if block else the most next statement after the if block
+#   print(x)
 #if-else
-if condition:
-  statement
-else: 
-  statement 
-  # if block executes if the if condition is true else else statement if be executed bothcan't at same time and else can't be used without else
+# if condition:
+#   statement
+# else: 
+#   statement 
+#   # if block executes if the if condition is true else else statement if be executed bothcan't at same time and else can't be used without else
 #if-elif
-if condition:
-  statement
-elif condition:
-  statement
-elif condition:
-  statement
-else:
-  statement 
- # when we want to check diffrerent condition parallely independently checking multiple things then we use if-elif-else statements 
+# if condition:
+#   statement
+# elif condition:
+#   statement
+# elif condition:
+#   statement
+# else:
+#   statement 
+#  # when we want to check diffrerent condition parallely independently checking multiple things then we use if-elif-else statements 
 
 #nested if else:
- if condition:
-   if condition:
-     statement
- else:
-   if condition:
-     statement
-    #using inner and outer if else and making it nested is using the indentation in python when we want multiple conditions to be checked dependently on the outer conditions 
+# if condition:
+#   if condition:
+#     statement
+# else:
+#   if condition:
+#     statement
+#    #using inner and outer if else and making it nested is using the indentation in python when we want multiple conditions to be checked dependently on the outer conditions 
 # printing the outputs 
 # using the keywords of python

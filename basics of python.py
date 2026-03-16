b32c317a3a7f5643beb23311aeb2d21de7d15468
mayankkumarlinghe-jpg <mayankkumarlinghe@gmail.com>
2026-03-16 12:00:00 +0530

    Fix IndentationError and NameError in basics of python

--- basics of python.py
+++ basics of python.py
@@ -9,18 +9,18 @@ print(type(3.00))
 print(type(True))
 print(type("String"))
 print(type([]))
-dict={}
-print(type(dict))
+my_dict={}
+print(type(my_dict))
 print(type(33/3))
 print(type(3.0/2))
 print(type(3!=2))
 print(type(3>=2))
 print(type(3>2.0))
 # taking input in python
-x=input("Enter a string input")
-y=int(input("Enter a integer input"))
-z=float(input("Enter a float input"))
-q=int(input("Enter a integer input"))
+x=input("Enter a string input: ")
+y=int(input("Enter a integer input: "))
+z=float(input("Enter a float input: "))
+q=int(input("Enter a integer input: "))
 print(x)
 print(y)
 print(z)
@@ -32,33 +32,38 @@ comment in python"""
 comments in python'''
 
 #conditional statements in python:
+
+# Adding mock variables so the code below executes without NameError
+condition = True
+statement = "Executing statement"
+
 # if statement
-# if condition:#if condition is true statement executes the if block else the most next statement after the if block
-#   print(x)
+if condition:#if condition is true statement executes the if block else the most next statement after the if block
+  print(x)
 #if-else
-# if condition:
-#   statement
-# else: 
-#   statement 
-#   # if block executes if the if condition is true else else statement if be executed bothcan't at same time and else can't be used without else
+if condition:
+  print(statement)
+else: 
+  print(statement)
+  # if block executes if the if condition is true else else statement if be executed bothcan't at same time and else can't be used without else
 #if-elif
-# if condition:
-#   statement
-# elif condition:
-#   statement
-# elif condition:
-#   statement
-# else:
-#   statement 
-#  # when we want to check diffrerent condition parallely independently checking multiple things then we use if-elif-else statements 
+if condition:
+  print(statement)
+elif condition:
+  print(statement)
+elif condition:
+  print(statement)
+else:
+  print(statement)
+ # when we want to check diffrerent condition parallely independently checking multiple things then we use if-elif-else statements 
 
 #nested if else:
-# if condition:
-#   if condition:
-#     statement
-# else:
-#   if condition:
-#     statement
-#    #using inner and outer if else and making it nested is using the indentation in python when we want multiple conditions to be checked dependently on the outer conditions 
+if condition:
+  if condition:
+    print(statement)
+else:
+  if condition:
+    print(statement)
+  #using inner and outer if else and making it nested is using the indentation in python when we want multiple conditions to be checked dependently on the outer conditions 
 # printing the outputs 
 # using the keywords of python
